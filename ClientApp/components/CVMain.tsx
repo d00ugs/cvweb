70fd5a9423dda1f02cd168a98853d529a86a8c66
Dougal Kennedy <Dougal.Kennedy@qnrl.com>
2018-01-23 13:39:35 +0000

    Added beginnings of CV

---
+++ ClientApp/components/CVMain.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+import 'isomorphic-fetch';
+
+interface CVMainDataState {
+    overview: CVOverview;
+    loading: boolean;
+}
+
+export class CVMain extends React.Component<RouteComponentProps<{}>, CVMainDataState> {
+    constructor() {
+        super();
+        this.state = { overview: '', loading: true };
+
+        fetch('localhost:8080/api/cv/overview')
+            .then(response => response.json() as Promise<CVOverview>)
+            .then(data => {
+                this.setState({ overview: data, loading: false });
+            });
+    }
+
+    public render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : CVMain.renderCVOverview(this.state.overview);
+
+        return <div>
+            <h1>Weather forecast</h1>
+            <p>This component demonstrates fetching data from the server.</p>
+            { contents }
+        </div>;
+    }
+
+    private static renderCVOverview(overview: CVOverview) {
+        return <p>
+            {overview.summary}</p>;
+    }
+}
+
+interface CVOverview {
+    summary: string;
+}
--- ClientApp/routes.tsx
+++ ClientApp/routes.tsx
@@ -3,10 +3,12 @@ import { Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
+import { CVMain } from './components/CVMain';
 import { Counter } from './components/Counter';
 
 export const routes = <Layout>
     <Route exact path='/' component={ Home } />
     <Route path='/counter' component={ Counter } />
     <Route path='/fetchdata' component={ FetchData } />
+    <Route path='/cvmain' component={ CVMain } />
 </Layout>;
